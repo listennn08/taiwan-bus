2ffb2603ef56529de14fe05255002e7c90b183dd
listennn08 <listennn08@outlook.com>
2022-03-12 14:11:40 +0800

    refactor: refactor info page logic

--- pages/info/[id].tsx
+++ pages/info/[id].tsx
@@ -1,115 +1,39 @@
-import { useRouter } from 'next/router'
 import { bindActionCreators, Dispatch } from 'redux'
 import { connect } from 'react-redux'
 import { RootState, wrapper } from '@/store'
 import { setCities, setCurrentCity, setIsLoading, setResultBusRoutes } from '@/store/search/action'
-import { getOperator, getSpecificRouteStop, getRouteShape, getRouteSchedule, getRouteByRouteName, getBusStopOfRoute, getEstimatedTimeOfArrival } from '@/pages/api'
+import { getOperator, getRouteShape, getRouteByRouteName } from '@/pages/api'
 import RouteInfo from '@/components/info/RouteInfo'
 import InfoHeader from '@/components/info/Header'
 import BusStatus from '@/components/info/BusStatus'
 import { replaceFromURI } from '@/utils'
 import { generateGeoStringToLatLngArray } from '@/apollo/resolvers/utils'
+import { getSchedule,  particularStopOfRoute, useBusInfo } from '@/logic/useBusInfo'
 
-
-type fnParams = { city: string, routeName: string, routeUID: string }
-
-async function particularStopOfRoute({ city, routeName, routeUID }: fnParams) {
-    const resp = (await getBusStopOfRoute({ 
-      city,
-      routeName,
-      filter: `RouteUID eq '${ routeUID}'`,
-    })).data
-    const estimatedTimeResp = (await getEstimatedTimeOfArrival({
-      city,
-      routeName,
-      filter: `RouteUID eq '${routeUID}'`,
-    })).data
-
-    for (let i = 0; i < resp.length; i += 1) {
-      for (let j = 0; j < estimatedTimeResp.length; j += 1) {
-        if (resp[i].Direction === estimatedTimeResp[j].Direction) {
-          const busStops = resp[i].Stops
-          resp[i].Stops = busStops.map((el) => {
-            if (el.StopUID  === estimatedTimeResp[j].StopUID) {
-              return {
-                ...el,
-                TimeInfo: estimatedTimeResp[j]
-              }
-            }
-            return el
-          })
-        }
-      }
-    }
-
-    return resp
-}
-
-async function getSchedule({ city, routeName, routeUID }: fnParams) {
-  const resp: IBusSchedule[] = (await getRouteSchedule({
-    city,
-    routeName,
-    filter: `RouteUID eq '${routeUID}'`
-  })).data
-  
-  let ServiceDay: string[] = []
-  if (resp[0]?.Frequencys) {
-    ServiceDay = Array.from(new Set(resp[0].Frequencys.flatMap((el) => {
-      return Object.keys(el.ServiceDay || {}).filter((key) => el.ServiceDay?.[key] === 1)
-    })))
-  } else if (resp[0]?.Timetables) {
-    ServiceDay = Array.from(new Set(resp[0].Timetables.flatMap((el) => {
-      return Object.keys(el.ServiceDay || {}).filter((key) => el.ServiceDay?.[key] === 1)
-    })))
-  }
-
-  return {
-    RouteUID: resp[0].RouteUID,
-    ServiceDay,
-  }
-}
-
-type TSchedule = { RouteUID: string, ServiceDay: string[] }
-interface IProps {
-  cityName: string
-  route: IBusRoute
-  routeInfo: IBusStopOfRoute[]
-  operator: TOperatorInfo[]
-  shape: IBusShape[]
-  schedule: TSchedule
-  setIsLoading: typeof setIsLoading
-  setResultBusRoutes: typeof setResultBusRoutes
-}
+import type { IProps, TSchedule } from '@/logic/useBusInfo'
 
 const InfoBus = (props: IProps) => {
-  const { cityName, route, operator, schedule, setIsLoading } = props
-  const [routeInfo, setRouteInfo] = useState(props.routeInfo)
-  const [shape, setShape] = useState(props.shape)
-  const router = useRouter()
-
-  const fetchData = useMemo(() => async () => {
-    const { id } = router.query as { id: string }
-    if (!id) return
-    const [city, routeName, routeUID] = Buffer.from(replaceFromURI(id), 'base64').toString('utf-8').split('_')
-    
-    setIsLoading(true)
-    const resp = (await particularStopOfRoute({ city, routeName, routeUID }))
-
-    setRouteInfo(resp)
-
-    const shapeResp = generateGeoStringToLatLngArray((await getRouteShape({ city, routeName, routeUID })).data)
-
-    setShape(shapeResp)
-    setIsLoading(false)
-  }, [router.query, setIsLoading])
-
+  const {
+    cityName,
+    route,
+    routeInfo,
+    operator,
+    schedule,
+    shape,
+    isOpen,
+    time,
+    setIsOpen,
+    refresh,
+  } = useBusInfo(props)
 
   return (
-    <div className="bg-light h-full overflow-y-scroll pb-10">
-      <div className="container mx-auto pt-15">
-        <InfoHeader cityName={cityName} route={route} routeInfo={routeInfo} fetchData={fetchData} />
-        <BusStatus route={route} routeInfo={routeInfo} shape={shape} />
-        <RouteInfo route={route} serviceDay={schedule.ServiceDay} operator={operator} />
+    <div className="
+      bg-gradient-to-b from-[#70EBB0] to-[#40CACA] md:bg-light h-full overflow-y-hidden md:overflow-y-scroll md:pb-10
+    ">
+      <div className="container mx-auto md:px-0 pt-2.5 md:pt-15">
+        <InfoHeader cityName={cityName} route={route} routeInfo={routeInfo} time={time} refresh={refresh} isOpen={isOpen} setIsOpen={setIsOpen} />
+        <BusStatus route={route} routeInfo={routeInfo} shape={shape} refresh={refresh}/>
+        <RouteInfo cityName={cityName} route={route} serviceDay={schedule.ServiceDay} operator={operator} isOpen={isOpen} setIsOpen={setIsOpen}/>
       </div>
     </div>
   )
@@ -146,16 +70,12 @@ export const getStaticProps =  wrapper.getStaticProps((store) => async (context)
 
     const particularRoutes = (await getRouteByRouteName({ city, routeName, routeUID })).data as IBusRoute[]
     setResultBusRoutes(particularRoutes)(store.dispatch)
+
     route = particularRoutes.find((el) => el.RouteName.Zh_tw === routeName)
-
     routeInfo = (await particularStopOfRoute({ city, routeName, routeUID }))
-    
     shape = generateGeoStringToLatLngArray((await getRouteShape({ city, routeName, routeUID })).data)
-    
     operator =  (await getOperator({ city, filter: `OperatorID eq '${operatorID}'` })).data
-
     schedule = (await getSchedule({ city, routeName, routeUID }))
-
   } catch (e) {
     isError = true
     console.log(e)
