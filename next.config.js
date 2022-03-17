c1b296932047e2e85b877e23235a3bac1a7a90a0
listennn08 <listennn08@outlook.com>
2022-03-17 23:08:07 +0800

    fix: remove pwa and workflow & update api keys

--- next.config.js
+++ next.config.js
@@ -1,25 +1,18 @@
-const withPWA = require('next-pwa')
 const WindiCSSWebpackPlugin = require('windicss-webpack-plugin')
 const Icons = require('unplugin-icons/webpack')
 const IconsResolver = require('unplugin-icons/resolver')
 const AutoImport = require('unplugin-auto-import/webpack')
 
 /** @type {import('next').NextConfig} */
-module.exports = withPWA({
-  pwa: {
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-    // disable: process.env.NODE_ENV === 'development',
-  },
+module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['raw.githubusercontent.com'], // next/image need add image domain
   },
   env: {
-    API_KEY: 'ten24bcf4362674a3435c934f9ef8b4470b91',
-    API_SECRET: 'EwLWG1SgD9rAjMMv-Dsyc0Bskp8',
-    BUS_API_PATH: 'https://ptx.transportdata.tw/MOTC/v2/Bus'
+    API_KEY: process.env.API_KEY,
+    API_SECRET: process.env.API_SECRET,
+    BUS_API_PATH: process.env.BUS_API_PATH,
   },
   webpack(config) {
     config.module.rules.push({
@@ -59,4 +52,4 @@ module.exports = withPWA({
 
     return config
   },
-})
+}
