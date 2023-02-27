8efbeea391145271323532e92c9a71bce2ab7693
Matt <listennn08@outlook.com>
2023-02-27 17:24:26 +0800

    chore: remove file

--- apollo/resolvers/station/index.ts
+++
@@ -1,9 +0,0 @@
-import { nearStations } from './near'
-import { stationsByKetword } from './search'
-
-export const stationQueries = {
-  Query: {
-    nearStations,
-    stationsByKetword,
-  }
-}
--- apollo/services/core/cityCore.ts
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-
-const cityInstance = axios.create({
-  baseURL: process.env.CITY_API_PATH!,
-})
-
-export const cityGet = () => cityInstance.get('/City?$format=JSON')
